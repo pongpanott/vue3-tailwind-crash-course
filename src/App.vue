e74bc4c24c167f90b6bce8a9ce747db6ca7d296d
pongpanott <fhyptr@gmail.com>
2022-03-25 01:44:11 +0700

    initital commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="text-center mt-[100px]">
+		<p class="text-teal-500 text-3xl">Hello Tailwind</p>
+		<p class="text-gray-500 mt-2">
+			Enjoy building your component from scratch.
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "App",
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
