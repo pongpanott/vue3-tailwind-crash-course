6ddc7d23d75b186e780c6485f957e7b5ee055c71
pongpanott <fhyptr@gmail.com>
2022-03-25 21:27:27 +0700

    setup project and components

---
+++ src/Views/HappyHacking.vue
@@ -0,0 +1,60 @@
+<template>
+	<AppLayout>
+		<div class="max-w-[600px] mx-auto w-full">
+			<div class="mb-6"><PrimaryButton /></div>
+			<form
+				@submit.prevent="onSubmit"
+				class="border-[1px] border-gray-200 rounded w-full p-4 flex flex-col"
+			>
+				<div class="flex flex-col text-left">
+					<!-- NOTE: style yout text field component here -->
+					<input
+						type="text"
+						placeholder="enter your name"
+						v-model="text"
+						@change="someHandler"
+					/>
+					<div class="min-h-[24px]">
+						<span v-if="error" class="text-red-500 text-xs"
+							>this field is required.</span
+						>
+					</div>
+				</div>
+
+				<div class="mt-4">
+					<button>submit</button>
+				</div>
+			</form>
+		</div>
+	</AppLayout>
+</template>
+
+<script>
+import AppLayout from "@/layout/AppLayout";
+import PrimaryButton from "@/components/PrimaryButton";
+
+export default {
+	name: "HappyHacking",
+	components: { AppLayout, PrimaryButton },
+	data() {
+		return {
+			text: "",
+			error: false,
+		};
+	},
+	methods: {
+		onSubmit() {
+			// console.log("submit", this.text);
+			if (this.text === "" || !this.text) {
+				this.error = true;
+			} else {
+				const yourName = {
+					name: this.text,
+				};
+
+				alert(JSON.stringify(yourName, null, 2));
+			}
+		},
+	},
+};
+</script>
